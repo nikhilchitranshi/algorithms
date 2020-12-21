8049f0102ecb15f284f8eb65821e2b17c9a18427
nikhilchitranshi <nikhilchitranshi@gmail.com>
2020-12-21 11:27:42 +0530

    2020-12-21

---
+++ src/com/algorithms/hashtables/SingleNumber.java
@@ -0,0 +1,38 @@
+package com.algorithms.hashtables;
+
+//Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+//
+//Follow up: Could you implement a solution with a linear runtime complexity and without using extra memory?
+//
+// 
+//
+//Example 1:
+//
+//Input: nums = [2,2,1]
+//Output: 1
+//Example 2:
+//
+//Input: nums = [4,1,2,1,2]
+//Output: 4
+//Example 3:
+//
+//Input: nums = [1]
+//Output: 1
+
+public class SingleNumber {
+	
+	public static void main(String[] args) {
+		int nums[] = {2,2,1};
+		
+		System.out.println(findSingleNumber(nums));
+	}
+
+	public static int findSingleNumber(int[] nums) {
+		int a = 0;
+		for (int i : nums) {
+			a ^= i;
+		}
+		return a;
+	}
+
+}
